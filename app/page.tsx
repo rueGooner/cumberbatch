ca04115ec3ac9807ba6fc9fdc54d03b91ed7a194
rue <rue.cumberbatch@bt.com>
2024-11-17 01:32:14 +0000

    feat: working particles and framer-motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,117 @@
 'use client';
+
+import { useState, useEffect, useMemo, useRef } from 'react';
 import { Navigation } from '@/app/components/Navigation';
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { Engine, Container, MoveDirection, OutMode, ISourceOptions } from '@tsparticles/engine';
+import { loadSlim } from '@tsparticles/slim';
+import { Background } from '@/app/components/Background';
 
 export default function Home() {
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine: Engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#dedede",
+        },
+        opacity: 0.4
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#6d28d9",
+        },
+        links: {
+          color: "#6d28d9",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: MoveDirection.none,
+          enable: true,
+          outModes: {
+            default: OutMode.out,
+          },
+          random: false,
+          speed: 2,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
   return (
     <div className='h-full flex flex-col w-full min-h-screen'>
-      <div className="background"></div>
+      <Background />
       <Navigation />
+      {init && (
+        <Particles
+          id="tsparticles"
+          options={options} // Apply the particle options here
+          particlesLoaded={particlesLoaded}
+        />
+      )}
     </div>
   );
 }
